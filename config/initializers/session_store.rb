a2eb6f62f3febf774dcd460acb1871dd286a3173
Jeff Dean <jeff@galvanize.it>
2015-02-04 21:12:54 -0700

    upgraded rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_g2-midterm-assessment_session'
+Rails.application.config.session_store :cookie_store, key: '_rails_practice_session'
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,11 @@ ActiveRecord::Schema.define(version: 20140721161538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "locations", force: true do |t|
+  create_table "locations", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "people", force: true do |t|
+  create_table "people", force: :cascade do |t|
     t.string   "title"
     t.string   "first_name"
     t.string   "last_name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20140721161538) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name",            null: false
     t.string "email",           null: false
     t.string "password_digest", null: false
